1aa90be32b2761e9b0bb00f69988b75351373777
A.M <a.movahedy82@gmail.com>
2025-12-01 11:13:43 +0330

    initial orientation

---
+++ parcking.cpp
@@ -0,0 +1,15 @@
+//class input; //with queue
+
+class linkList; 
+class stack; //with link list
+class parcking; //array of stacks
+class car;   //linked list with node
+
+//functions:
+//void input(void ){};
+//void input(int ){};
+
+//int find(int ){};
+//void output(int ){};
+void sortOfStack(int ){};
+void displacement(int i, int j){};
