11afbb0723155d69a796d635c2af7700cba7a79c
A.M <a.movahedy82@gmail.com>
2025-12-03 14:49:51 +0330

    add 2 functions: output and displacement.

--- newParking.cpp
+++ newParking.cpp
@@ -65,6 +65,13 @@ public:
         return true;
     }
     
+     int getFirst() {
+        if (head == nullptr) {
+            return -1;  // Or throw exception
+        }
+        return head->data;
+    }
+
     void clear() {
         while (head != nullptr) {
             Node* temp = head;
@@ -93,27 +100,31 @@ class Stack{
         if(size == capacity) return true;
         else return false;
     }
-    void push(int carID){
-        if (isFull) cout << "stack is full."<< endl;
+    bool push(int carID){
+        if (isFull()){
+            cout << "stack is full."<< endl;
+            return false;}
         else{
-            Node* newNode = new Node(carID);
             list.addFirst(carID);
             size ++;
+            return true;
         }
         
     }
     int pop(){
-        if (isEmpty) cout << "stack is empty." <<endl;
+        if (isEmpty()) cout << "stack is empty." <<endl;
         else{
+            int carID = list.getFirst();
             list.removeFirst();
             size --;
-            int carID = list.head->data;
             return carID;
         }
     }
 
     int peek(){
-        if (isEmpty) cout << "stack is empty." <<endl;
+        if (isEmpty){ 
+            cout << "stack is empty." <<endl;
+            return 0;}
         else{
             int carID = list.head->data;
             return carID;
@@ -125,18 +136,73 @@ class Queue{};
 
 class Parking{
     private:
-
-    //Node car;
-    vector<Stack> column;
-    int numbersOfColumn,columnCapacity;
+    vector<Stack> columns;
+    int numbersOfColumn;
+    int columnCapacity;
 
     public:
-    // Constructor
     Parking(int n, int m) : numbersOfColumn(n), columnCapacity(m){
-        for (int i=0; i<=numbersOfColumn; i++)  column.push_back(Stack(columnCapacity));
+        for (int i=0; i < numbersOfColumn; i++)  columns.push_back(Stack(columnCapacity));
         cout << "Parking created with " << numbersOfColumn
              << " columns, each of capacity " << columnCapacity << endl;
     }
+
+    
+    int input(int carID){
+
+    }
+
+    void input(int carID,int i){
+
+    }
+
+    int find(int carID){
+
+    }
+
+    bool output(int carID){
+        for(int i=0; i<=numbersOfColumn; i++){
+            if(carID == columns[i].peek()){
+                columns[i].pop();
+                cout << "Car " << carID << " removed from column " << i << endl;
+                return true;
+            }
+        }
+        cout << "Car " << carID << " is not at top." << endl;
+        return false;
+    }
+
+    void ordering(int i){
+
+    }
+
+    void displacement(int i, int j){
+        int d = j;
+        int r=0,s=0;
+        while(columns[i].isEmpty()!= true){
+            if (columns[d].isFull() && d != numbersOfColumn-1){
+                d++;
+                s++;
+                continue;
+            }
+            if (columns[d].isFull() && d == numbersOfColumn-1){
+                d=0;
+                continue;
+                s++;
+            }
+            if (columns[d].isFull() && d == i){
+                d++;
+                continue;
+            }
+            if(s == numbersOfColumn-2){
+                cout << "i try all of stacks and we didnt have enough empty space." << endl;
+                break;
+            }
+            columns[j].push(columns[i].pop());
+        }
+        if (columns[i].peek()==0) cout << "displacement done." <<endl;
+    }
+    
 };
 
 class Car{
@@ -151,4 +217,16 @@ class Car{
     }
 };
 
-int main(){}
+void inputCar(Parking& parking, int carID, int column);
+int findCar(Parking& parking, int carID);
+bool outputCar(Parking& parking, int carID);
+void orderColumn(Parking& parking, int column);
+void displaceCars(Parking& parking, int fromCol, int toCol);
+
+int main(){
+    int n, m;
+    cin>>n>>m;
+    Parking(n,m);
+
+    
+}
